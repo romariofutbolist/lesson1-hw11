d5fbd285964e4ca85c22b3dd971c02efbc4a4eb0
romariofutbolist <66all@mail.ru>
2023-07-03 20:46:59 +0300

    поправка ДЗ

--- src/Main.java
+++ src/Main.java
@@ -9,53 +9,40 @@ public class Main {
     }
 
     public static void printYear(int year) {
-        System.out.println(year + " год - високосный год");
-    }
-
-    public static void task1() {
-        System.out.println("Задача 1");
-        int year = 1601;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            printYear(year);
+            System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год - невисокосный год");
         }
     }
 
-    public static int checkOS(int clientOS) {
+    public static void task1() {
+        System.out.println("Задача 1");
+        int year = 1600;
+        printYear(year);
+        }
+
+    public static void checkPhone(int checkIOs, int checKYear) {
         int iOs = 0;
         int android = 1;
-        if (clientOS == iOs) {
-            clientOS = iOs;
-        } else if (clientOS == android) {
-            clientOS = android;
+        int year = 2015;
+        if (checKYear >= year && checkIOs == iOs) {
+            System.out.println("Установите версию приложения для iOs по ссылке");
+        } else if (checKYear < year && checkIOs == iOs) {
+            System.out.println("Установите облегченную версию приложения для iOs по ссылке");
+        } else if (checKYear >= year && checkIOs == android) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (checKYear < year && checkIOs == android) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
-        return clientOS;
-    }
-
-    public static int checkYear(int clientDeviceYear) {
-        int currentYear = LocalDate.now().getYear();
-        if (clientDeviceYear == currentYear) {
-            clientDeviceYear = 1;
-        } else {
-            clientDeviceYear = 0;
-        }
-        return clientDeviceYear;
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        int clientOS = 0, clientDeviceYear = 2022;
-        int phoneOS = checkOS(clientOS);
-        int yearPhone = checkYear(clientDeviceYear);
-        if (phoneOS == 1 && yearPhone == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (phoneOS == 1 && yearPhone != 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (phoneOS == 0 && yearPhone == 1) {
-            System.out.println("Установите версию приложения для iOs по ссылке");
-        } else if (phoneOS == 0 && yearPhone != 1) {
-            System.out.println("Установите облегченную версию приложения для iOs по ссылке");
+        int currentYear = LocalDate.now().getYear();
+        int clientOS = 2;
+        if (clientOS == 0 || clientOS == 1) {
+        checkPhone(clientOS, currentYear);
         } else {
             System.out.println("Операцмонная система не поддерживается");
         }
@@ -63,22 +50,21 @@ public class Main {
 
     private static int checkDeliveryDay (int distance){
         int day = 0;
-        if (distance<20) {
+        if (distance < 20) {
             day ++;
-        } else if (20<=distance&&distance<60) {
-            day +=2;
-        } else if (60<=distance&&distance<=100) {
-            day +=3;
-        } else {
-            day+=4;
+        } else if (20 <= distance && distance < 60) {
+            day += 2;
+        } else if (60 <= distance && distance <= 100) {
+            day += 3;
         }
         return day;
     }
+
     private static void task3() {
         System.out.println("задача 3");
         int deliveryDistance = 60;
         int deliveryDays = checkDeliveryDay(deliveryDistance);
-        if(deliveryDays<=3){
+        if (deliveryDays <= 3){
             System.out.println("Потребуется дней: " + (deliveryDays));
         } else {
             System.out.println("доставки нет");
